3c98b2b6d44f9004085d2aa84774fefc06c199bf
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-09-30 09:34:28 +0600

    completed the detailed post page design

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,7 +1,27 @@
+import { Link } from 'react-router-dom';
+import DetailedPost from './DetailedPost/DetailedPost';
+import RelatedPosts from './RelatedPosts/RelatedPosts';
+
 export default function Blog() {
 	return (
-		<div>
-			<h1 className='text-center'>Blog</h1>
+		<div className='w-11/12 mx-auto'>
+			{/* <!-- Go Home / Go Back --> */}
+			<div className='container mt-8'>
+				<Link
+					to='/'
+					className='inline-block text-gray-600 home-btn'
+					id='lws-goHome'>
+					<i className='mr-2 fa-solid fa-house'></i>Go Home
+				</Link>
+			</div>
+
+			<section className='post-page-container'>
+				{/* <!-- detailed post  --> */}
+				<DetailedPost />
+
+				{/* <!-- related posts --> */}
+				<RelatedPosts />
+			</section>
 		</div>
 	);
 }
